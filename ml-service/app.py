811cee2214b56a5583466937ec66f26b0f6878e9
zild <bilaldi393@gmail.com>
2026-07-17 14:20:25 +0700

    Refactor app.py to use environment variable for port and update requirements.txt with specific package versions

--- ml-service/app.py
+++ ml-service/app.py
@@ -155,10 +155,14 @@ def predict():
         }), 500
 
 
+import os
+
 if __name__ == "__main__":
+
+    port = int(os.environ.get("PORT", 5000))
 
     app.run(
         host="0.0.0.0",
-        port=5000,
-        debug=True
+        port=port,
+        debug=False
     )
